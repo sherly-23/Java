0f1a5fb470e9b1cfa8a999ed9e94c950ee99417a
sherly-23 <sherlyyamarthy@gmail.com>
2025-04-09 14:24:52 +0530

    save to master

--- Add.java
+++ Add.java
@@ -5,5 +5,7 @@ public class Add {
         int b=5;
         int sum=a+b;
         System.out.println("Addition:"+sum);
+        int sub=a-b;
+        System.out.println("Subtraction:"+sub);
     }
 }
